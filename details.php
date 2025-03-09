e7b48d2c060fc9a60e13edb6fcce35259bb72fad
Peiyu Han <hanpeiyu7755@gmail.com>
2025-03-08 19:28:15 -0500

    add gitignore file and details php

--- details.php
+++ details.php
@@ -19,7 +19,7 @@ curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);gg
 
 $result = curl_exec($ch);
 curl_close($ch);
